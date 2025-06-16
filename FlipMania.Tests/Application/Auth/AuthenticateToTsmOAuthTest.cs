109ce19d6e9aa0bca700bb376819baa8dc86b1fb
Golluméo <p.mauriello54@gmail.com>
2025-06-16 11:21:11 +0200

    test(application): add AuthenticateToTsmOAuthTest
    Introduces a unit test class `AuthenticateToTsmOAuthTest` under the Tests project. This sets up the structure for testing TSM OAuth authentication with placeholders for credentials and a test method.

---
+++ FlipMania.Tests/Application/Auth/AuthenticateToTsmOAuthTest.cs
@@ -0,0 +1,13 @@
+﻿namespace FlipMania.Tests.Application.Auth;
+
+public class AuthenticateToTsmOAuthTest
+{
+    private string _clientId = "client-test";
+    private string _apiKey = "api-key-test";
+
+    [Fact]
+    public void CanLogToTsmApiWithValidCredentials()
+    {
+        // 
+    }
+}
