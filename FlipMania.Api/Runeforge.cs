13c6fbf9c8c73cece541d9a1179b52a62fca4fb3
Golluméo <p.mauriello54@gmail.com>
2025-06-16 10:41:33 +0200

    feat(api): add `Runeforge` class
    Introduces the `Runeforge` class in FlipMania.Api to extend `DormantRuneforge`. Implements a `Frostmourne` method, currently yielding no additional runes, to support future API enhancements.

---
+++ FlipMania.Api/Runeforge.cs
@@ -0,0 +1,13 @@
+﻿using Rituals.Contracts;
+using Rituals.Runeforge;
+
+namespace FlipMania.Api;
+
+public class Runeforge : DormantRuneforge
+{
+    protected override IEnumerable<IRune> Frostmourne()
+    {
+        // no additional rune to load
+        yield break;
+    }
+}
